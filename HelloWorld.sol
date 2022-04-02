e7741fbb84a0321a7c1b3cbc5bfe7ff040dafb66
Quatre.Finance <35783747+bobeu@users.noreply.github.com>
2022-04-03 00:20:47 +0100

    Initial commit

---
+++ HelloWorld.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+//Simple Hello World Smart contract that stores and retrieve unsigned integer value;
+contract HelloWorld {
+  ///@dev State variable declaration: number
+  uint private number;
+  
+  /**@dev Updates state (number)
+    @param _number - new unsigned integer
+  */ 
+  function storeNumber(uint _number) public {
+    number = _number;
+  }
+
+  /**@dev retrieves number from storage
+    @return _number unsigned integer value
+  */
+  function retrieveNumber() public view returns(uint _number){
+    _number = number;
+    return _number;
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # assessment
+Simple hello world smart contract that stores and retrieve an unsigned integer value.
+
+### Deploy using Remix
+
+- "# Screen snip"
